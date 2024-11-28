62d7091528443979b3c5aa2304c2a5d3b5cac3fd
Pedro Sanchez Gil <pedrosanchezgilg@icloud.com>
2024-11-28 14:04:11 -0600

    update with VenueDetailView

---
+++ DininGuru/RatingService.swift
@@ -0,0 +1,8 @@
+//
+//  RatingService.swift
+//  DininGuru
+//
+//  Created by Pedro Sánchez-Gil Galindo on 11/28/24.
+//
+
+import Foundation
---
+++ DininGuru/VenueDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  VenueDetailView.swift
+//  DininGuru
+//
+//  Created by Pedro Sánchez-Gil Galindo on 11/28/24.
+//
+
+import Foundation
