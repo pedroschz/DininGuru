c36a1abbc829ba0fa08461991e82cbf4c5146f2f
Pedro Sanchez Gil <pedrosanchezgilg@icloud.com>
2024-11-30 20:31:51 -0500

    Removal of .env files

--- diningguru_backend/diningguru_backend/settings.py
+++ diningguru_backend/diningguru_backend/settings.py
@@ -14,6 +14,22 @@ from pathlib import Path
 from datetime import timedelta
 
 
+from decouple import config
+
+# Load variables from .env
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback-secret-key')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,10 +161,3 @@ SIMPLE_JWT = {
 }
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "thepipatel@gmail.com"
-EMAIL_HOST_PASSWORD = "nfksvtedzhtmvtwj"
-DEFAULT_FROM_EMAIL = 'noreply@example.com'
